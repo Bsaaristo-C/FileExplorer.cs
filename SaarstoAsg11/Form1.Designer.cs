014746cc4975586480b4eab2c20ce79fe9fb798c
Bsaaristo <92605542+Bsaaristo@users.noreply.github.com>
2022-12-09 18:31:14 -0600

    Add files via upload

---
+++ SaarstoAsg11/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace SaarstoAsg11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxWITCCourses = new System.Windows.Forms.GroupBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxGrade = new System.Windows.Forms.TextBox();
+            this.textBoxCredits = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBoxWITCCourses.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBoxWITCCourses
+            // 
+            this.groupBoxWITCCourses.Controls.Add(this.buttonSave);
+            this.groupBoxWITCCourses.Controls.Add(this.buttonNext);
+            this.groupBoxWITCCourses.Controls.Add(this.buttonPrevious);
+            this.groupBoxWITCCourses.Controls.Add(this.label4);
+            this.groupBoxWITCCourses.Controls.Add(this.label3);
+            this.groupBoxWITCCourses.Controls.Add(this.label2);
+            this.groupBoxWITCCourses.Controls.Add(this.label1);
+            this.groupBoxWITCCourses.Controls.Add(this.textBoxGrade);
+            this.groupBoxWITCCourses.Controls.Add(this.textBoxCredits);
+            this.groupBoxWITCCourses.Controls.Add(this.textBoxName);
+            this.groupBoxWITCCourses.Controls.Add(this.textBoxNumber);
+            this.groupBoxWITCCourses.Location = new System.Drawing.Point(105, 61);
+            this.groupBoxWITCCourses.Name = "groupBoxWITCCourses";
+            this.groupBoxWITCCourses.Size = new System.Drawing.Size(566, 251);
+            this.groupBoxWITCCourses.TabIndex = 0;
+            this.groupBoxWITCCourses.TabStop = false;
+            this.groupBoxWITCCourses.Text = "WITC Courses";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(414, 203);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(90, 31);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(168, 203);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(90, 31);
+            this.buttonNext.TabIndex = 9;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(62, 203);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(90, 31);
+            this.buttonPrevious.TabIndex = 8;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Grade:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Credits:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number:";
+            // 
+            // textBoxGrade
+            // 
+            this.textBoxGrade.Location = new System.Drawing.Point(213, 168);
+            this.textBoxGrade.Name = "textBoxGrade";
+            this.textBoxGrade.Size = new System.Drawing.Size(141, 22);
+            this.textBoxGrade.TabIndex = 3;
+            // 
+            // textBoxCredits
+            // 
+            this.textBoxCredits.Location = new System.Drawing.Point(213, 128);
+            this.textBoxCredits.Name = "textBoxCredits";
+            this.textBoxCredits.Size = new System.Drawing.Size(141, 22);
+            this.textBoxCredits.TabIndex = 2;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(213, 85);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(141, 22);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(213, 44);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(141, 22);
+            this.textBoxNumber.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Brian Saaristo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBoxWITCCourses);
+            this.Name = "Form1";
+            this.Text = "Saaristo Asg 11 Courses";
+            this.groupBoxWITCCourses.ResumeLayout(false);
+            this.groupBoxWITCCourses.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxWITCCourses;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxGrade;
+        private System.Windows.Forms.TextBox textBoxCredits;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SaarstoAsg11/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static SaarstoAsg11.Form1;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SaarstoAsg11
+{
+    public partial class Form1 : Form
+    {
+        // Create a stack to store the student data
+        Stack<Course> Courses = new Stack<Course>();
+        Stack<Course> CoursesNext = new Stack<Course>();
+        public Form1()
+        {
+            InitializeComponent();
+            Course newCourse = new Course();
+
+            newCourse = new Course(10152100, "Database Concepts and SQL", 3, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152101, "Web Design and Development", 3, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152102, "Advanced Web Site Development", 3, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152106, "Java Programming - Beginning", 3, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152107, "Java Programming - Advanced", 2, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152108, "Enterprise Java Programming", 2, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152110, "Programming in SQL", 3, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152111, "Systems Analysis and Design", 3, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152112, "Server - Side Web Development", 3, "NA");
+            Courses.Push(newCourse);
+            newCourse = new Course(10152113, "Applications Development", 3, "NA");
+            Courses.Push(newCourse);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            int number = 0;
+            string name = "" ;
+            double gpa = 0.0;
+            string grade = "";
+            Course newCourse = new Course();
+
+
+
+            try
+            { 
+            // Get the student data from the user
+            number = int.Parse(textBoxNumber.Text);
+                }
+            catch (Exception) {
+                textBoxNumber.Text = "Enter valid input";
+                    }
+            /////////////////////////////////////////////////////////////
+            try { 
+            name = textBoxName.Text;
+                }
+            catch (Exception) { 
+                textBoxName.Text = "Enter valid input";
+                }
+            /////////////////////////////////////////////////////////////
+            try { 
+            gpa = double.Parse(textBoxCredits.Text);
+                }
+            catch (Exception)
+            {
+                textBoxCredits.Text = "Enter a valid input";
+            }
+            /////////////////////////////////////////////////////////////
+            try { 
+            grade = textBoxGrade.Text;               
+                }
+            catch (Exception)
+            {
+                textBoxGrade.Text = "Enter a valid input";
+            }
+            if (textBoxGrade.Text == "")
+                textBoxGrade.Text = "Enter a valid input";
+            if (textBoxName.Text == "")
+                textBoxName.Text = "Enter a valid input";
+
+
+            // Create a new Student structure with the input data
+            newCourse = new Course(number, name, gpa, grade);
+
+            // Push the student onto the stack
+            Courses.Push(newCourse);
+
+
+        }
+        public struct Course
+        {
+            public int number;
+            public string name;
+            public double gpa;
+            public string grade;
+
+            // Constructor that takes four parameters
+            public Course(int number, string name, double gpa, string grade)
+            {
+                this.number = number;
+                this.name = name;
+                this.gpa = gpa;
+                this.grade = grade;
+            }
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+
+           if (Courses.Count > 0)
+    {
+        // Use the Peek() method to get the top item in the stack
+        Course top = Courses.Peek();
+
+        // Print the previous item in the stack
+        textBoxNumber.Text = Courses.Peek().number.ToString();
+        textBoxName.Text = Courses.Peek().name;
+        textBoxCredits.Text = Courses.Peek().gpa.ToString();
+        textBoxGrade.Text = Courses.Peek().grade;
+
+        // Push the top item back onto the stack
+        CoursesNext.Push(top);
+
+        // Use the Pop() method to remove the top item from the stack
+        Courses.Pop();
+    }
+
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+           if (CoursesNext.Count > 0)
+    {
+        // Print the next item in the stack
+        textBoxNumber.Text = CoursesNext.Peek().number.ToString();
+        textBoxName.Text = CoursesNext.Peek().name;
+        textBoxCredits.Text = CoursesNext.Peek().gpa.ToString();
+        textBoxGrade.Text = CoursesNext.Peek().grade;
+
+        // Push the top item from the CoursesNext stack onto the Courses stack
+        Courses.Push(CoursesNext.Peek());
+
+        // Use the Pop() method to remove the top item from the CoursesNext stack
+        CoursesNext.Pop();
+    }
+
+
+        }
+    }
+}
+
